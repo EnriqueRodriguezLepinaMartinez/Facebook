85a45d702c189abed2f3a0511b5f1dcba8f14f1f
erodriguez-lepinamartinez <erodriguez-lepinamartinez@nautilus22.danielcastelao.org>
2017-04-24 10:08:20 +0200

    Facebook

---
+++ src/appfacebook/AppFacebook.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package appfacebook;
+
+import facebook4j.Facebook;
+import facebook4j.FacebookFactory;
+
+/**
+ *
+ * @author erodriguez-lepinamartinez
+ */
+public class AppFacebook {
+
+    /**At first it is necessary to acquire Facebook instance to use Facebook4J.
+    You can get Facebook instance in FacebookFactory.getInstance().
+    */
+    Facebook facebook = new FacebookFactory().getInstance();
+    
+    
+    
+    https://unpocodejava.wordpress.com/2014/10/07/un-poco-de-facebook4j-facebook-java/
+    http://facebook4j.github.io/en/code-examples.html
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    }
+    
+}
